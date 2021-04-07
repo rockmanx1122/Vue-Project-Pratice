7b558d426fce51543c93a51f3bc1825d94d34ab6
黄鸿 <925657822@qq.com>
2021-04-07 13:48:06 +0800

    构建小米官网项目

---
+++ xiaomi/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export const views=[
+    {
+
+    }
+]
+
+
+const router=()=>new Router({
+views
+})
+
+export default router
